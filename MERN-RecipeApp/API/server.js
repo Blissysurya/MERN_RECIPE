b36344e0061109a56b8b9cf72424f5a9e597e6d6
Blissysurya <suryanshdixit4924.dpsjkp@gmail.com>
2024-11-03 00:39:23 +0530

    hi

---
+++ MERN-RecipeApp/API/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from 'express'
+import userRouter from './routes/user.js'
+import recipeRouter from './routes/recipe.js'
+import cors from 'cors'
+const app = express(); 
+
+app.use(bodyParser.json())
+app.use(cors({
+  origin:true,
+  methods:["GET","POST","PUT","DELETE"],
+  credentials:true
+ 
+}))
+
+// userRouter
+app.use('/api',userRouter)
+
+// recipeRouter
+app.use('/api',recipeRouter)
+
+mongoose
+  .connect(
+    "mongodb+srv://suryanshdixit4924dpsjkp:kxQPfTI33koe9smm@cluster0.dyem3.mongodb.net/",
+    {
+      dbName: "MERN_Recipe_YouTube",
+    }
+  )
+  .then(() => console.log("MongoDB is Connected..!"))
+  .catch((err) => console.log(err.message));
+
+const port = 3000;
+app.listen(port, () => console.log(`server is running on port ${port}`));
+// username=suryanshdixit4924dpsjkp
+// password=kxQPfTI33koe9smm
