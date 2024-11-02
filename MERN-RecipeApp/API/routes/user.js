0fbda16a9431998045b728971ebc2007bf9c82f9
Blissysurya <suryanshdixit4924.dpsjkp@gmail.com>
2024-11-03 01:30:49 +0530

    h

---
+++ MERN-RecipeApp/API/routes/user.js
@@ -0,0 +1,22 @@
+import express from 'express'
+// import { register,login, profile } from '../controllers/user.js';
+// import { Authenticate } from '../middlewares/auth.js';
+
+const router = express.Router();
+
+// user register
+router.post("/register", (req,res)=>{
+    const {name,gmail,password}=req.body;
+
+    console.log(req.body);
+});
+
+// user login
+router.post('/login',login)
+
+// profile
+
+router.get('/user',Authenticate,profile)
+
+
+export default router
--- MERN-RecipeApp/API/server.js
+++ MERN-RecipeApp/API/server.js
@@ -6,13 +6,13 @@ import recipeRouter from './routes/recipe.js'
 import cors from 'cors'
 const app = express(); 
 
-app.use(bodyParser.json())
-app.use(cors({
-  origin:true,
-  methods:["GET","POST","PUT","DELETE"],
-  credentials:true
+// app.use(bodyParser.json())
+// app.use(cors({
+//   origin:true,
+//   methods:["GET","POST","PUT","DELETE"],
+//   credentials:true
  
-}))
+// }))
 
 // userRouter
 app.use('/api',userRouter)
