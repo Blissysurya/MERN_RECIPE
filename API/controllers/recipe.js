55cdbc7975226bc02541ee1c328b25348c1bf5ef
Blissysurya <150290655+Blissysurya@users.noreply.github.com>
2024-12-18 19:48:44 +0530

    as

--- API/controllers/recipe.js
+++ API/controllers/recipe.js
@@ -1,5 +1,5 @@
 import { Recipe } from "../Models/Recipe.js";
-
+import {SavedRecipe} from '../Models/SavedRecipe.js'
 export const add=async (req,res)=>{
     const {title,ist,ing1,ing2,ing3,ing4,qty1,qty2,qty3,qty4,imgurl}=req.body;
 
@@ -16,4 +16,51 @@ export const add=async (req,res)=>{
 export const getAllRecipe = async (req,res) =>{
         const recipe=await Recipe.find();
         res.json({recipe});
+}
+
+export const getRecipeById = async (req,res) =>{
+
+        const id = req.params.id;
+
+        try{
+                let recipe=await Recipe.findById(id);
+
+                if(!recipe) res.json({message:"recipe not exists"})
+
+                res.json({message:"recipeById",recipe})
+        }catch(error){
+                res.json({message:error})
+        }       
+}
+
+export const getRecipeByUserId = async (req,res) => {
+        const userId = req.params.id;
+
+        try{
+                let recipe=await Recipe.find({user:userId});
+
+                if(!recipe) res.json({message:"recipe not exists"})
+
+                res.json({message:"recipe by user Id",recipe})
+        }catch(error){
+                res.json({message:error})
+        }  
+}
+
+export const savedRecipeById = async (req,res) =>{
+        const id=req.params.id
+        let recipe= await SavedRecipe.findOne({recipe:id});
+
+        if(recipe) return res.json({message:"recipe already saved"})
+
+        recipe=await SavedRecipe.create({recipe:id});
+
+        res.json({message:"Recipe saved successfully"})
+
+}
+
+export const getSavedRecipe= async (req,res) =>{
+        const recipe= await SavedRecipe.find();
+
+        res.json({recipe})
 }
--- API/routes/recipe.js
+++ API/routes/recipe.js
@@ -1,9 +1,20 @@
 import express from "express";
-import { add, getAllRecipe } from "../controllers/recipe.js";
+import { add, getAllRecipe, getRecipeById, getRecipeByUserId, getSavedRecipe, savedRecipeById } from "../controllers/recipe.js";
 const router=express.Router();
 
 router.post('/add',add)
 
 router.get('/',getAllRecipe)
+
+router.get('/saved',getSavedRecipe)
+
+router.get('/:id',getRecipeById)
+
+
+
+router.get('/user/:id',getRecipeByUserId)
+
+router.post('/:id',savedRecipeById);
+
 
 export default router
